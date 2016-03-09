efda4a1766e1b154264139c8329393f7ea964e9c
joshcodes <josh@blackbarlabs.com>
2016-03-08 21:08:41 -0600

    added message as optional constructor to NoRecEx

--- NoRecordException.cs
+++ NoRecordException.cs
@@ -4,5 +4,14 @@ namespace BlackBarLabs.Persistence
 {
     public class NoRecordException : Exception
     {
+        public NoRecordException() : base()
+        {
+
+        }
+
+        public NoRecordException(string message) : base(message)
+        {
+
+        }
     }
 }
